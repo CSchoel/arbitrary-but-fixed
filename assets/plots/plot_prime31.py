01883ee31be8b5e4b844fcf860b4c51c6b433507
Christopher Schölzel <christopher.schoelzel@gmx.net>
2022-04-07 18:48:59 +0200

    adds proper colors

--- assets/plots/plot_prime31.py
+++ assets/plots/plot_prime31.py
@@ -1,6 +1,6 @@
 import pathlib
 from bokeh.plotting import figure, show
-from bokeh.palettes import Bokeh7
+from bokeh.palettes import Category10_10
 import numpy as np
 import pandas as pd
 import itertools as it
@@ -11,12 +11,12 @@ def plot_prime31():
     """
     Plots 2D scatter plot of successive values in sample data
     """
-    colors = Bokeh7
+    colors = Category10_10
     data = pd.read_csv(cwd / "prime31.csv", delimiter=";")
     experiments = [x for x in data.columns.values if x not in ["prime", "time"]]
     f = figure(x_range=(2, 500), title="Collision probabilities for different primes", x_axis_label='prime', y_axis_label='collisions [%]')
-    for e, c in zip(experiments, Bokeh7):
-        f.line("prime", e, color=c, source=data)
+    for e, c in zip(experiments, colors):
+        f.line("prime", e, color=c, source=data, legend_label=e, line_width=2)
 
     show(f)
 
