6413f2c7a632527f1beac3a550a9d1784ca99030
Christopher Schölzel <christopher.schoelzel@gmx.net>
2022-04-07 17:06:39 +0200

    adds plot for prime31 example

---
+++ assets/plots/plot_prime31.py
@@ -0,0 +1,22 @@
+from bokeh.plotting import figure, show
+from bokeh.models.formatters import DatetimeTickFormatter
+import pathlib
+import datetime
+import numpy as np
+import pandas as pd
+
+cwd = pathlib.Path(__file__).parent
+
+def plot_prime31():
+    """
+    Plots 2D scatter plot of successive values in sample data
+    """
+    data = pd.read_csv(cwd / "prime31.csv")
+    experiments = [x for x in data.columns.values if x not in ["prime", "time"]]
+    f = figure(title="Collision probabilities for different primes", x_axis_label='prime', y_axis_label='collisions [%]')
+    for e in experiments:
+        f.line("prime", e, source=data)
+    show(f)
+
+if __name__ == "__main__":
+    plot_prime31()
