f2e60586524746b321beda781baad9f7b4639ac2
Christopher Schölzel <christopher.schoelzel@gmx.net>
2022-04-07 17:55:46 +0200

    uses vbar instead of line

--- assets/plots/plot_prime31.py
+++ assets/plots/plot_prime31.py
@@ -1,7 +1,9 @@
 import pathlib
 from bokeh.plotting import figure, show
+from bokeh.models import FactorRange
 import numpy as np
 import pandas as pd
+import itertools as it
 
 cwd = pathlib.Path(__file__).parent
 
@@ -11,9 +13,12 @@ def plot_prime31():
     """
     data = pd.read_csv(cwd / "prime31.csv", delimiter=";")
     experiments = [x for x in data.columns.values if x not in ["prime", "time"]]
-    f = figure(title="Collision probabilities for different primes", x_axis_label='prime', y_axis_label='collisions [%]')
-    for e in experiments:
-        f.line("prime", e, source=data)
+    # for e in experiments:
+    #     f.line("prime", e, source=data)
+    x = [(str(p), e) for p, e in it.product(data["prime"], experiments)]
+    y = sum([tuple(v) for v in data[experiments].values], ())
+    f = figure(x_range=FactorRange(*x), title="Collision probabilities for different primes", x_axis_label='prime', y_axis_label='collisions [%]')
+    f.vbar(x=x, top=y)
     show(f)
 
 if __name__ == "__main__":
